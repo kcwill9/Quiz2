132f0e4f0b63a4771144b755cb5bdbb720498e17
Kai Williams <kcwill9@ilstu.edu>
2025-11-06 15:29:35 -0600

    Update Quiz2.ino

--- Quiz2.ino
+++ Quiz2.ino
@@ -1,8 +1,9 @@
+#importing methods
 import time
 import random
 from gpiozero import LED, Button
 
-
+#assigning button and LED outputs on the GPIO
 rButton = Button(5)
 gButton = Button(6)
 bButton = Button(13)
@@ -11,6 +12,7 @@ rLED = LED(16)
 gLED = LED(25)
 bLED = LED(21)
 
+#main code to turn the LEDs off and on
 def main():
 
     if rButton.is_pressed:
